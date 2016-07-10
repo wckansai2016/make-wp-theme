7112205cb0bb9699e1d50668da0064b1324d3a54
kurudrive <kurudrive@gmail.com>
2016-07-10 11:58:42 +0900

    bodyタグが２つあったので１つ削除...

--- wck-lesson-complete/index.php
+++ wck-lesson-complete/index.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>WordCampKansai</title>
-    <?php wp_head();?>
-  </head>
-
-  <body>
-
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>WordCampKansai</title>
+  <?php wp_head();?>
+</head>
  <body>
  <header class="wck_head">
  <div class="container">
